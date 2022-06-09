0e79c899c8a2cbf9ab6300053880ef638582d84f
Nate Hanson <natehanson@REACTIONs-Mac-mini.local>
2022-06-09 17:03:48 -0600

    Email body size and buttons

--- frontend/src/components/inputs/input_fields/TextEdit/TextEdit.jsx
+++ frontend/src/components/inputs/input_fields/TextEdit/TextEdit.jsx
@@ -18,6 +18,16 @@ export const TextEdit = () => {
     return "not-handled";
   }
 
+  //this is if we want to have more custom functionality
+  const customStyleMap = {
+    STRIKETHROUGH: {
+      textDecoration: "line-through"
+    },
+    FONT_SIZE_30: {
+      fontSize: "30px"
+    }
+  };
+
   function _onBoldClick() {
     setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"));
   }
@@ -27,15 +37,22 @@ export const TextEdit = () => {
   function _onStrikethroughClick() {
     setEditorState(RichUtils.toggleInlineStyle(editorState, "STRIKETHROUGH"));
   }
+  function _onUnderlineClick() {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
+  }
   return (
     <div className={styles.editor}>
-      <button onClick={_onBoldClick.bind(this)}>B</button>
-      <button onClick={_onItalicClick.bind(this)}>I</button>
-      <button onClick={_onStrikethroughClick.bind(this)}>Strike</button>
+      <div className={styles.editbuttons}>
+        <button className={styles.singleletter} onClick={_onBoldClick.bind(this)}>B</button>
+        <button className={styles.singleletter} onClick={_onItalicClick.bind(this)}>I</button>
+        <button className={styles.singleletter} onClick={_onStrikethroughClick.bind(this)}><strike>S</strike></button>
+        <button className={styles.singleletter} onClick={_onUnderlineClick.bind(this)}><u>U</u></button>
+      </div>
       <Editor
         editorState={editorState}
         onChange={setEditorState}
         handleKeyCommand={handleKeyCommand}
+        customStyleMap={customStyleMap}
       />
     </div>
   );
