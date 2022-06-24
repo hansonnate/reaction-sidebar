ac2c6c553ae84a64ec06a420d326c4bd65653433
SpencerLaudie <spencer.laudie@gmail.com>
2022-06-24 16:36:24 -0600

    Creates image upload component

---
+++ frontend/src/components/inputs/input_fields/ImageUpload/ImageUpload.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from "react";
+import { Stack } from "@mui/material";
+import styles from "./ImageUpload.module.scss";
+
+export const ImageUpload = ({ onUpload, imageFile }) => {
+  const [file, setFile] = useState(imageFile);
+  const imageInput = useRef();
+
+  const handleFileChange = (event) => {
+    console.log("setting file to ", event.target.files[0].name);
+    setFile(event.target.files[0]);
+    if (onUpload) {
+      onUpload(file);
+    }
+  };
+
+  const handleRemoveFile = () => {
+    setFile(null);
+    imageInput.current.value = null;
+    if (onUpload) {
+      onUpload(null);
+    }
+  };
+
+  return (
+    <Stack direction="row" className="align-items-center" spacing={1}>
+      <label>
+        <input
+            ref={imageInput}
+          accept="image/*"
+          type="file"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+          
+        />
+        <span className={styles.button}>
+          <i className="bi bi-upload" />
+          &nbsp;&nbsp;Upload
+        </span>
+      </label>
+
+      <div className={styles.fileNameContainer}>
+        {file ? (
+          <>
+            <div className={styles.fileName}>{file.name}</div>
+            <div onClick={handleRemoveFile} className={styles.deleteButton}>
+              <i className="bi bi-x" />
+            </div>
+          </>
+        ) : (
+          "No image selected"
+        )}
+      </div>
+    </Stack>
+  );
+};
